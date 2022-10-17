fc4cdb8c75e624e1c618e2961c8bbd97b5386379
Robbie McKinstry <robbie@pulumi.com>
2022-10-17 19:08:16 -0400

    Split writes across files

--- pkg/codegen/nodejs/gen.go
+++ pkg/codegen/nodejs/gen.go
@@ -1797,13 +1797,17 @@ func (ns *namespace) genOwnedTypes(ctx *ioContext, dirRoot string) (*ioFile, err
 		var isInputType = ctx.input && ctx.mod.details(t).inputType
 		var isOutputType = !ctx.input && ctx.mod.details(t).outputType
 		// Only write input and output types.
-		if isInputType || isOutputType {
+		if isInputType {
 			if err := ctx.mod.genType(file.writer(), t, ctx.input, 0); err != nil {
 				return file, err
 			}
-			if i != len(ns.types)-1 {
-				fmt.Fprintf(file.writer(), "\n")
+		} else if isOutputType {
+			if err := ctx.mod.genType(file.writer(), t, ctx.input, 0); err != nil {
+				return file, err
 			}
+		}
+		if i != len(ns.types)-1 && (isInputType || isOutputType) {
+			fmt.Fprintf(file.writer(), "\n")
 		}
 	}
 	return file, nil
